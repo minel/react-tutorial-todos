903c49f0aac741edfe34a80bd1235660bf0bf91f
Mücahit İNEL <mucahit.inel@hotmail.com>
2016-11-09 22:54:13 +0300

    list todos items

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ React ToDos App with ES6
 
 [![Browserify](https://img.shields.io/badge/build-passing-brightgreen.svg)]() [![npm](https://img.shields.io/badge/license-ISG-blue.svg)]()
 
-##Install Requirements
-Install webpack and webpack-dev-server globally
+##Requirements
+You should install webpack and webpack-dev-server globally
 ```bash
 npm install -g webpack
 npm install -g webpack-dev-server
@@ -22,4 +22,4 @@ Start dev server
 webpack-dev-server
 ```
 
-Open your browser and go localhost:8080
+Now, open your browser and go http://localhost:8080
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import TodosList from './todos-list';
 
 export default class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            todos
+        };
+    }
+
     render() {
         return (
             <div>
                 <h1>React ToDos App</h1>
+                <TodosList todos={this.state.todos} />
             </div>
         );
     }
 }
+
+
+const todos = [
+    {
+        task: 'make React tutorial',
+        isCompleted: false
+    },
+    {
+        task: 'eat lunch',
+        isCompleted: true
+    }
+];
